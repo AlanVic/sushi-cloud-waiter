616afd023b139859e64a3c77fdf6b440c9594bf4
eliasPaulinoAndrade <eliaspaulinoandrade@gmail.com>
2019-01-28 12:02:15 -0300

    creating facade

---
+++ sushi-cloud-winter/Services/CloudKit/CloudKitFacade.swift
@@ -0,0 +1,15 @@
+//
+//  CloudKitFacade.swift
+//  sushi-cloud-winter
+//
+//  Created by Elias Paulino on 28/01/19.
+//  Copyright © 2019 Alan Victor Paulino de Oliveira. All rights reserved.
+//
+
+import Foundation
+
+struct CloudKitFacade {
+    func sendOrder(plates: [Plates]) {
+        
+    }
+}
