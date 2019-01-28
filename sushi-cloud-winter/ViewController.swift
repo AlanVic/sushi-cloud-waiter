f461bc56458ba766fa29fbd4e0db7524d5e040d5
Alan Victor Alan <alanvictor5447@gmail.com>
2019-01-28 10:51:07 -0300

    Initial Commit

---
+++ sushi-cloud-winter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sushi-cloud-winter
+//
+//  Created by Alan Victor Paulino de Oliveira on 28/01/19.
+//  Copyright © 2019 Alan Victor Paulino de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ sushi-cloud-winterTests/sushi_cloud_winterTests.swift
@@ -0,0 +1,34 @@
+//
+//  sushi_cloud_winterTests.swift
+//  sushi-cloud-winterTests
+//
+//  Created by Alan Victor Paulino de Oliveira on 28/01/19.
+//  Copyright © 2019 Alan Victor Paulino de Oliveira. All rights reserved.
+//
+
+import XCTest
+@testable import sushi_cloud_winter
+
+class sushi_cloud_winterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
